9075be9d5be1be250a4f8c8e07f384ca94d18652
Luan Vale <luancabral5566@outlook.com>
2022-02-07 19:20:18 -0300

    Add project files.

---
+++ Exercicio01.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio01.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool fecharApp = false;
+
+            while (fecharApp == false) 
+            {
+                Console.WriteLine("===== Checar soma =====");
+                Console.WriteLine("");
+                Console.WriteLine("Programa para calcular se a soma de A + B resulta menor que C.");
+                Console.WriteLine("");
+
+                Console.Write("Digite o valor de A: ");
+                string inputA = Console.ReadLine();
+                int inputANum = int.Parse(inputA);
+                Console.WriteLine("");
+
+                Console.Write("Digite o valor de B: ");
+                string inputB = Console.ReadLine();
+                int inputBNum = int.Parse(inputB);
+                Console.WriteLine("");
+
+                Console.Write("Digite o valor de C: ");
+                string inputC = Console.ReadLine();
+                int inputCNum = int.Parse(inputC);
+                Console.WriteLine("");
+            } 
+        }
+    }
+}
